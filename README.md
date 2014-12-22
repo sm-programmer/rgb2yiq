e0d2eef72b32402546f2c1977145ce1c378ebe1e
sm-programmer <sm-programmer@users.noreply.github.com>
2014-12-21 19:25:56 -0600

    Change main files to adhere to GNU GPL v3

--- README.md
+++ README.md
@@ -2,11 +2,27 @@ rgb2yiq
 =======
 
 RGB to YIQ PIL-based image converter for Python 3.x
+Copyright (C) 2014	sm-programmer
 
 Objective
 =========
 
 This project aims to develop software capable of converting any type of images into special YIQ image files (for now using a custom file structure).
+
+YIQ files are interesting when it comes to obtaining compatible B/W images with no extra processing, whilst preserving colour information as well.
+
+This small program demonstrates its usefulness as it only requires Pillow and a Python 3 interpreter to run properly.
+
+How to use
+==========
+
+From a terminal, type the following:
+
+python3 rgb2yiq inputimgfile.ext
+
+It currently supports any image type supported by Pillow.
+
+The program generates a file as output, named `inputimgfile.ext.yiq', with the structure described below.
 
 File structure
 ==============
@@ -21,3 +37,23 @@ Finally, in sequential order, triplets of (Y, I, Q) values are stored for each p
 
 - Y value is stored as an integer, 0 <= Y <= 100
 - I and Q are rounded and stored as integers, 0 <= I, Q <= 255
+
+To Do
+=====
+
+1. Implement parameter-checks and a better parameter management system.
+2. Generate output file to standard output for more flexibility.
+3. Implement reverse-direction YIQ -> image file.
+
+License
+=======
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
--- rgb2yiq.py
+++ rgb2yiq.py
@@ -1,45 +1,72 @@
 """
-	RGB2YIQ
-	Version: 1.0
-	December 1st, 2014
-	
-	For Python 3
+	RGB2YIQ: RGB to YIQ Pillow-based conversion program for Python 3.x
+	Copyright (C) 2014	sm-programmer
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 from PIL import Image
 from struct import *
 import sys
 
-if len(sys.argv) > 1:
-	useimg = sys.argv[1]
+print("RGB2YIQ: RGB to YIQ Pillow-based conversion program for Python 3.x")
+print("Copyright (C) 2014\tsm-programmer\n")
+
+if len(sys.argv) == 1:
+	print("This program is free software: you can redistribute it and/or modify")
+	print("it under the terms of the GNU General Public License as published by")
+	print("the Free Software Foundation, either version 3 of the License, or")
+	print("(at your option) any later version.\n")
+	print("This program is distributed in the hope that it will be useful,")
+	print("but WITHOUT ANY WARRANTY; without even the implied warranty of")
+	print("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the")
+	print("GNU General Public License for more details.\n")
+	print("You should have received a copy of the GNU General Public License")
+	print("along with this program.  If not, see <http://www.gnu.org/licenses/>.\n")
+	print("Usage:\n\tpython3 rgb2yiq <inimgfile>")
 else:
-	useimg = input("\nWrite input image file:\n")
+	print("This program comes with ABSOLUTELY NO WARRANTY; for details type `rgb2yiq'.")
+	print("This is free software, and you are welcome to redistribute it")
+	print(" under certain conditions; type `rgb2yiq' for details.")
+	useimg = sys.argv[1]
 
-try:
-	img = Image.open(useimg)
-	print("\nName:", useimg)
-	print("Format:", img.format)
-	print("Size:", img.size[0], "X", img.size[1])
-	print("Colour model:", img.mode, "\n")
-	img_rgb = img.convert('RGB')
 	try:
-		target = open(useimg + ".yiq", "wb")
-		target.write("YIQ1".encode('utf-8'))
-		target.write(pack('LL', img.size[0], img.size[1]))
-		target.write("DATA".encode('utf-8'))
-		pix = img_rgb.load()
-		for y in range(0, img.size[1]):
-			for x in range(0, img.size[0]):
-				r, g, b = pix[x, y]
-				r /= 255
-				g /= 255
-				b /= 255
-				fY = r * 0.30 + g * 0.59 + b * 0.11
-				fI = r * 0.599 - g * 0.2773 - b * 0.3217
-				fQ = r * 0.213 - g * 0.5251 + b * 0.3121
-				target.write(pack('bbb', round(fY*100), round(fI*100), round(fQ*100)))
+		img = Image.open(useimg)
+		print("\nName:", useimg)
+		print("Format:", img.format)
+		print("Size:", img.size[0], "X", img.size[1])
+		print("Colour model:", img.mode, "\n")
+		img_rgb = img.convert('RGB')
+		try:
+			target = open(useimg + ".yiq", "wb")
+			target.write("YIQ1".encode('utf-8'))
+			target.write(pack('LL', img.size[0], img.size[1]))
+			target.write("DATA".encode('utf-8'))
+			pix = img_rgb.load()
+			for y in range(0, img.size[1]):
+				for x in range(0, img.size[0]):
+					r, g, b = pix[x, y]
+					r /= 255
+					g /= 255
+					b /= 255
+					fY = r * 0.30 + g * 0.59 + b * 0.11
+					fI = r * 0.599 - g * 0.2773 - b * 0.3217
+					fQ = r * 0.213 - g * 0.5251 + b * 0.3121
+					target.write(pack('bbb', round(fY*100), round(fI*100), round(fQ*100)))
 
-		target.close()
+			target.close()
+		except:
+			print("Can't create output file!")
 	except:
-		print("Can't create output file!")
-except:
-	print(useimg, ": image not found")
+		print(useimg, ": image not found")
